2902830fd39afaa92b1fbd68513c578bf6c175eb
wangdxnum1 <Wang_Wonderful@foxmail.com>
2016-05-06 18:15:03 +0800

    添加style类

--- TWScrollViewController/TWScrollViewController/TWSegmentStyle.h
+++ TWScrollViewController/TWScrollViewController/TWSegmentStyle.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface TWSegmentStyle : NSObject
 
@@ -18,4 +19,49 @@
 
 // 是否缩放文字
 @property (nonatomic, assign) BOOL scaleTitle;
+
+// 是否可以滚动标题
+@property (nonatomic, assign) BOOL scrollTitle;
+
+// 是否颜色渐变
+@property (nonatomic, assign) BOOL gradualChangeTitleColor;
+
+// 是否显示附加的按钮
+@property (nonatomic, assign) BOOL showExtraButton;
+
+@property (nonatomic, copy) NSString *extraBtnBackgroundImageName;
+
+// 下面的滚动条的高度 默认2
+@property (nonatomic, assign) CGFloat scrollLineHeight;
+
+// 下面的滚动条的颜色
+@property (nonatomic, strong) UIColor *scrollLineColor;
+
+// 遮盖的背景颜色
+@property (nonatomic, strong) UIColor *coverBackgroundColor;
+
+// 遮盖圆角
+@property (nonatomic, assign) CGFloat coverCornerRadius;
+
+// cover的高度 默认28
+@property (nonatomic, assign) CGFloat coverHeight;
+
+// 文字间的间隔 默认15
+@property (nonatomic, assign) CGFloat titleMargin;
+
+// 文字 字体 默认14.0
+@property (nonatomic, strong) UIFont *titleFont;
+
+// 放大倍数 默认1.3
+@property (nonatomic, assign) CGFloat titleBigScale;
+
+// 默认倍数 不可修改
+@property (nonatomic, assign,readonly) CGFloat titleOriginalScale;
+
+// 文字正常状态颜色 请使用RGB空间的颜色值!! 如果提供的不是RGB空间的颜色值就可能crash
+@property (nonatomic, strong) UIColor *normalTitleColor;
+
+// 文字选中状态颜色 请使用RGB空间的颜色值!! 如果提供的不是RGB空间的颜色值就可能crash
+@property (nonatomic, strong) UIColor *selectedTitleColor;
+
 @end
--- TWScrollViewController/TWScrollViewController/TWSegmentStyle.m
+++ TWScrollViewController/TWScrollViewController/TWSegmentStyle.m
@@ -10,4 +10,28 @@
 
 @implementation TWSegmentStyle
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.showCover = NO;
+        self.showLine = NO;
+        self.scaleTitle = NO;
+        self.scrollTitle = YES;
+        self.gradualChangeTitleColor = NO;
+        self.showExtraButton = NO;
+        self.scrollLineHeight = 2.0;
+        self.scrollLineColor = [UIColor brownColor];
+        self.coverBackgroundColor = [UIColor lightGrayColor];
+        self.coverCornerRadius = 14.0;
+        self.coverHeight = 28.0;
+        self.titleFont = [UIFont systemFontOfSize:14.0];
+        self.titleBigScale = 1.3;
+        _titleOriginalScale = 1.0;
+        self.normalTitleColor = [UIColor colorWithRed:51.0/255.0 green:53.0/255.0 blue:75.0/255.0 alpha:1.0];
+        self.selectedTitleColor = [UIColor colorWithRed:255.0/255.0 green:0.0/255.0 blue:121.0/255.0 alpha:1.0];
+    }
+    return self;
+}
+
 @end
